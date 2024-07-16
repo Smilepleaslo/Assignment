2f410265fafb3afb45b3c39ad268762f7e3ee7da
Smilepleaslo <suchitstha13@gmail.com>
2024-07-16 15:55:08 +1000

    Part 2
    Construction of child class general practitioner

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -31,4 +31,36 @@ public class HealthProfessional {
         System.out.println("Name: " + name);
         System.out.println("Doctor Type: " + doctortype);
     }
+}
 
+public class GeneralPractitioner extends HealthProfessional {
+    private String clinicname;
+    private String qualifications;
+
+    public GeneralPractitioner() {
+        super();
+        this.clinicname = "";
+        this.qualifications = "";
+    }
+
+    public GeneralPractitioner(int id, String name, String doctortype, String clinicname, String qualifications) {
+        super(id, name, doctortype);
+        this.clinicname = clinicname;
+        this.qualifications = qualifications;
+    }
+
+    public String getClinicName() {
+        return clinicname;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    @Override
+    public void PrintDetails() {
+        super.PrintDetails();
+        System.out.println("Clinic Name: " + clinicname);
+        System.out.println("Qualifications: " + qualifications);
+    }
+}

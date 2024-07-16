da15a333b7c3d5b26810433d383a73d5817e4378
Smilepleaslo <suchitstha13@gmail.com>
2024-07-16 13:29:06 +1000

    Instance Variables

---
+++ src/AssignmentOne.java
@@ -0,0 +1,17 @@
+public class HealthProfessional {
+    private int id;
+    private String name;
+    private String doctortype;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDoctortype() {
+        return doctortype;
+    }
+    

e3efa72f0a200faab987e32285b44dfff8e72aa1
Smilepleaslo <suchitstha13@gmail.com>
2024-07-16 21:26:38 +1000

    Part 3
    Creating Objects

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,3 +1,12 @@
 public class AssignmentOne{
 //part 3-Using classes and objects
+
+    public static void main(String[] args) {
+        GeneralPractitioner gp1 = new GeneralPractitioner(1, "Dr. Shrestha", "General Practitioner", "MBBS");
+        GeneralPractitioner gp2 = new GeneralPractitioner(2, "Dr. Shakya", "General Practitioner", "B.Med");
+        GeneralPractitioner gp3 = new GeneralPractitioner(3, "Dr. Prajapati", "General Practitioner", "B.Surg");
+
+        Dentist dentist1 = new Dentist(4, "Dr. Karki", "Dentist", "DMD");
+        Dentist dentist2 = new Dentist(5, "Dr. Sebanja", "Dentist", "DDS");
+    }
 }

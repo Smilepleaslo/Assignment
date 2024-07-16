c08122cf0f46ead08a1aaaa7943caf4559674a1c
Smilepleaslo <suchitstha13@gmail.com>
2024-07-16 21:47:15 +1000

    Part 4
    Setting variables

---
+++ src/Appointment.java
@@ -0,0 +1,38 @@
+public class Appointment{
+    private String patientname;
+    private int patientphone;
+    private String prefferedtime;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPreferredTimeSlot() {
+        return preferredTimeSlot;
+    }
+
+    public void setPreferredTimeSlot(String preferredTimeSlot) {
+        this.preferredTimeSlot = preferredTimeSlot;
+    }
+
+    public HealthProfessional getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(HealthProfessional doctor) {
+        this.doctor = doctor;
+    }
+}
+
